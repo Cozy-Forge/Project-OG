482b2db467c0b44d182730cd60a9ea53789201c8
준1 <98933515+Wizone06@users.noreply.github.com>
2023-12-22 09:48:58 +0900

    뭔갈 했어
    뭔갈 했어

--- Assets/02_Script/Map/MapCheck.cs
+++ Assets/02_Script/Map/MapCheck.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class MapCheck : MonoBehaviour
 {
-    
+    [SerializeField] private GameObject Lload;
+    [SerializeField] private GameObject LWall;
+
+    [SerializeField] private GameObject Road;
+    [SerializeField] private GameObject RWall;
+
+    [SerializeField] private GameObject Upload;
+    [SerializeField] private GameObject UpWall;
+
+    [SerializeField] private GameObject Download;
+    [SerializeField] private GameObject DownWall;
+
+
+    private void Start()
+    {
+        
+    }
 }
